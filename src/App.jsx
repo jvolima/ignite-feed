7aa3c3f952946b38bedf8c4ad546f52b19ef5a68
João Vitor Lima <jvolima2004@gmail.com>
2022-07-21 20:03:18 -0300

    css global

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import { Post } from "./Post"
+import { Header } from "./components/Header";
+import { Post } from "./Post";
+
+import "./global.css";
 
 export function App() {
   return (
     <div>
+      <Header />
+
       <Post 
         author="João Vitor" 
         content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam soluta similique porro cupiditate vitae consequatur laboriosam consectetur facere repellendus numquam, voluptate corrupti nesciunt doloremque error deserunt ducimus itaque, repudiandae culpa." 
