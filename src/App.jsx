9d7cff4f8340bbf17d913459e479e5a1e759c56d
João Vitor Lima <jvolima2004@gmail.com>
2022-07-22 11:10:12 -0300

    componente sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,27 @@ import { Post } from "./Post";
 
 import "./global.css";
 
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
+
 export function App() {
   return (
     <div>
       <Header />
 
-      <Post 
-        author="João Vitor" 
-        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam soluta similique porro cupiditate vitae consequatur laboriosam consectetur facere repellendus numquam, voluptate corrupti nesciunt doloremque error deserunt ducimus itaque, repudiandae culpa." 
-      />
-      <Post 
-        author="Giovani Ocan" 
-        content="Um novo post muito legal." 
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="João Vitor" 
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam soluta similique porro cupiditate vitae consequatur laboriosam consectetur facere repellendus numquam, voluptate corrupti nesciunt doloremque error deserunt ducimus itaque, repudiandae culpa." 
+          />
+          <Post 
+            author="Giovani Ocan" 
+            content="Um novo post muito legal." 
+          />
+        </main>
+      </div>
     </div>
   )
 }
