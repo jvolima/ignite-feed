67c8a566b1e7aa2fc78c97d88fdd9dd1f64fefa1
João Vitor Lima <jvolima2004@gmail.com>
2022-07-22 17:52:04 -0300

    Começando componente Post

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Header } from "./components/Header";
-import { Post } from "./Post";
+import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
 
 import "./global.css";
 
 import styles from "./App.module.css";
-import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
---
+++ src/components/Post.jsx
@@ -0,0 +1,30 @@
+import styles from "./Post.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/jvolima.png" />
+          <div className={styles.authorInfo}>
+            <strong>João Vitor Lima</strong>
+            <span>Web developer</span>
+          </div>
+        </div>
+
+        <time title="22 de Julho às 13:24h" dateTime="2022-07-22 13:24:00">Publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+          <p>Fala galeraa 👋</p>
+          <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+          <p>👉 <a href="">jane.design/doctorcare</a></p>
+          <p>
+            <a href="">#novoprojeto</a>{' '} 
+            <a href="">#nlw</a>{' '} 
+            <a href="">#rocketseat</a>
+          </p>
+      </div>
+    </article>
+  )
+}
