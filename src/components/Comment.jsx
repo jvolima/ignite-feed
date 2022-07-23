5be94dad719c8f8bd42feedcf7ba288ae1bdf649
João Vitor Lima <jvolima2004@gmail.com>
2022-07-23 10:45:37 -0300

    Componente Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/jvolima.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>João Vitor Lima</strong>
+              <time title="22 de Julho às 13:24h" dateTime="2022-07-22 13:24:00">Cerca de 1h atrás</time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp size={20} />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
