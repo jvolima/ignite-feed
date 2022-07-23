db6130b4be09fbaa625b3a9032a5dece9cc95c14
João Vitor Lima <jvolima2004@gmail.com>
2022-07-23 12:29:07 -0300

    Componente Avatar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,14 @@
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
+import { Avatar } from "./Avatar";
 
 export function Post() {
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/jvolima.png" />
+          <Avatar src="https://github.com/jvolima.png" />
+
           <div className={styles.authorInfo}>
             <strong>João Vitor Lima</strong>
             <span>Web developer</span>
